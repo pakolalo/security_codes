b560ae3c17baa87398c69fa690ebf3014255b159
Isco del Collado <fedelcollado@gmail.com>
2026-06-15 12:45:31 -0600

    feat(*): add state to the components [ISCO-260615]

--- src/components/ClassState/index.js
+++ src/components/ClassState/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 
 class ClassState extends React.Component {
+    constructor() {
+        super();
+        this.state = { error: false }
+    }
     render() {
         return (
             <div>
-                <h2>Delete ClassState</h2>
+                <h2>Delete { this.props.name }</h2>
                 <p>Please write the security code.</p>
+                { this.state.error && (
+                    <p>Error: Invalid security code.</p>
+                )}
                 <div>
                     <input placeholder="Security Code" />
-                    <button>Check</button>
+                    <button
+                    onClick={() => this.setState({ error: !this.state.error})}
+                    >Check</button>
                 </div>
             </div>
-        )
+        );
     }
 }
 
--- src/components/UseState/index.js
+++ src/components/UseState/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
 
-function UseState() {
+function UseState({ name }) {
+    const [error, setError] = React.useState(false);
+
     return (
         <div>
-                <h2>Delete UseState</h2>
-                <p>Please write the security code.</p>
-                <div>
-                    <input placeholder="Security Code" />
-                    <button>Check</button>
-                </div>
+            <h2>Delete { name }</h2>
+            <p>Please write the security code.</p>
+            { error && (
+                <p>Error: Invalid security code.</p>
+            )}
+            <div>
+                <input placeholder="Security Code" />
+                <button
+                onClick={() => setError(!error)}
+                >Check</button>
             </div>
+        </div>
     );
 }
 
