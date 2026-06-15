c851be0ad8063a673c5211abefe100b75af4ea91
Isco del Collado <fedelcollado@gmail.com>
2026-06-15 14:24:03 -0600

    feat(*): add lifecycle in ClassState [ISCO-260615]

--- src/components/ClassState/index.js
+++ src/components/ClassState/index.js
@@ -1,22 +1,43 @@
 import React from 'react';
+import { Loading } from '../Loading/index';
 
 class ClassState extends React.Component {
     constructor() {
         super();
-        this.state = { error: false }
+        this.state = { error: false, loading: false };
     }
+
+    // UNSAFE_componentWillMount() {
+
+    // };
+
+    // componentDidMount() {
+
+    // };
+
+    componentDidUpdate() {
+        if(this.state.loading) {
+            setTimeout(() => {
+            this.setState({ loading: false });
+            }, 3000);
+        }
+    };
+
     render() {
         return (
             <div>
                 <h2>Delete { this.props.name }</h2>
                 <p>Please write the security code.</p>
+                { this.state.loading && (
+                    <Loading />
+                )}
                 { this.state.error && (
                     <p>Error: Invalid security code.</p>
                 )}
                 <div>
                     <input placeholder="Security Code" />
                     <button
-                    onClick={() => this.setState({ error: !this.state.error})}
+                    onClick={() => this.setState({ loading: true})}
                     >Check</button>
                 </div>
             </div>
