992446330da1ffff1854f9eeba4cf31fd900c510
Isco del Collado <fedelcollado@gmail.com>
2026-06-15 13:34:10 -0600

    feat(index.js): add useEffect into the component [ISCO-260615]

--- src/components/UseState/index.js
+++ src/components/UseState/index.js
@@ -2,6 +2,15 @@ import React from 'react';
 
 function UseState({ name }) {
     const [error, setError] = React.useState(false);
+    const [loading, setLoading] = React.useState(false);
+
+    React.useEffect(() => {
+        if(loading) {
+            setTimeout(() => {
+            setLoading(false);
+            }, 3000);
+        }
+    }, [loading]);
 
     return (
         <div>
@@ -10,10 +19,15 @@ function UseState({ name }) {
             { error && (
                 <p>Error: Invalid security code.</p>
             )}
+            { loading && (
+                <p>Loading...</p>
+            )}
             <div>
                 <input placeholder="Security Code" />
                 <button
-                onClick={() => setError(!error)}
+                onClick={() => {
+                    setLoading(true);
+                }}
                 >Check</button>
             </div>
         </div>
