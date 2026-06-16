2f97ed76df98d6ae36cf22797fb879f0088a8d2e
Isco del Collado <fedelcollado@gmail.com>
2026-06-16 10:56:43 -0600

    feat(index.js): fix loading state [ISCO-260616]

--- src/components/UseState/index.js
+++ src/components/UseState/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 
+const SECURITY_CODE = 'paradigma';
+
 function UseState({ name }) {
+    const [value, setValue] = React.useState('');
     const [error, setError] = React.useState(false);
     const [loading, setLoading] = React.useState(false);
 
     React.useEffect(() => {
         if(loading) {
             setTimeout(() => {
-            setLoading(false);
-            }, 3000);
+                if(value !== SECURITY_CODE) {
+                    setError(true);
+                }
+                setLoading(false);
+            }, 2000);
         }
     }, [loading]);
 
@@ -16,14 +22,20 @@ function UseState({ name }) {
         <div>
             <h2>Delete { name }</h2>
             <p>Please write the security code.</p>
-            { error && (
+            { (error && !loading) && (
                 <p>Error: Invalid security code.</p>
             )}
             { loading && (
                 <p>Loading...</p>
             )}
             <div>
-                <input placeholder="Security Code" />
+                <input 
+                placeholder="Security Code" 
+                value={value}
+                onChange={(event) => {
+                    setValue(event.target.value);
+                }}
+                />
                 <button
                 onClick={() => {
                     setLoading(true);
